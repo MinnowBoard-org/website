63c95d64c9dbf3b8c556d6a98732e3419490f43c
James Ketrenos <james.p.ketrenos@intel.com>
2016-11-29 11:11:42 -0800

    Do not serve update.log
    Signed-off-by: James Ketrenos <james.p.ketrenos@intel.com>

--- app.js
+++ app.js
@@ -37,11 +37,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get(basePath + 'messages.log', function(req, res, next) {
-  res.status(401).send("Unauthorized.");
-});
-app.get(basePath + '.git/?*', function(req, res, next) {
-  res.status(401).send("Unauthorized.");
+/* Return 401's for the following path-regexps */
+[ 'messages.log', '.git/?*', 'update.log' ].forEach(function(pattern) {
+  app.get(basePath + pattern, function(req, res, next) {
+    res.status(401).send("Unauthorized.");
+  });
 });
 
 app.use(basePath + 'bower_components', express.static(path.join(__dirname, 'bower_components')));
